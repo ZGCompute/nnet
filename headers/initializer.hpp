0f2fb949e2fbeacb4dbc2138c673842e0691f866
ZacharyIG <zacharyigreenberg@gmail.com>
2018-03-15 13:21:27 -0700

    virtual destructor

--- headers/initializer.hpp
+++ headers/initializer.hpp
@@ -5,33 +5,43 @@
 #include <map>
 #include <list>
 #include <math.h>
+
 #include <opencv/cv.h>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/core/core.hpp>
 
 using namespace std;
 
-#ifndef _INITIALIZER
-#define _INITIALIZER
-#include "initializer.cpp"
-
+#ifndef INITIALIZER_HPP
+#define INITIALIZER_HPP
 
 // Deep fully-connected neural network, and Deep Convolutional netowrk implementation using OpenCV & OpenCL backend 
 // compile with: "g++ nnet.cpp -o nnet -I headers/ -std=c++11 `pkg-config opencv --cflags --libs`"
 
 class initializer {
-
   
 public:
-
   
   // init Constructor and destructor
   initializer();
- ~initializer();
-
+  virtual ~initializer();
 
 private:
 
+  // Arguments:
+  // layer_dims -- 2d vector containing the size of each layer.
+  // Returns:
+  // parameters -- dictionary (map) containing your parameters
+  //               as c++ vectors of 3d (for conv layers) or 2d (for fc layers)
+  //               cv::Mat "W1", "b1", ..., "WL", "bL":
+  // Fully connected example wieghts:
+  // W1 -- weight matrix of shape (layers_dims[1], layers_dims[0])
+  // b1 -- bias vector of shape (layers_dims[1], 1)
+  // WL -- weight matrix of shape (layers_dims[L], layers_dims[L-1])
+  // bL -- bias vector of shape (layers_dims[L], 1)
+  // FIXME: add example of conv2d wight shapes
+  map< string, vector< cv::Mat > > parameters \
+  init_params_he( vector< vector< int > > layer_dims, vector< string > layer_types ); 
 
   // Arguments:
   // layer_dims -- 2d vector containing the size of each layer.
@@ -46,7 +56,7 @@ private:
   // bL -- bias vector of shape (layers_dims[L], 1)
   // FIXME: add example of conv2d wight shapes
   map< string, vector< cv::Mat > > parameters \
-  initializer::init_params_he( vector< vector< int > > layer_dims, vector< string > layer_types ); 
+  init_params_rand( vector< vector< int > > layer_dims, vector< string > layer_types );
 
   // Arguments:
   // layer_dims -- 2d vector containing the size of each layer.
@@ -61,23 +71,7 @@ private:
   // bL -- bias vector of shape (layers_dims[L], 1)
   // FIXME: add example of conv2d wight shapes
   map< string, vector< cv::Mat > > parameters \
-  initializer::init_params_rand( vector< vector< int > > layer_dims, vector< string > layer_types );
-
-  // Arguments:
-  // layer_dims -- 2d vector containing the size of each layer.
-  // Returns:
-  // parameters -- dictionary (map) containing your parameters
-  //               as c++ vectors of 3d (for conv layers) or 2d (for fc layers)
-  //               cv::Mat "W1", "b1", ..., "WL", "bL":
-  // Fully connected example wieghts:
-  // W1 -- weight matrix of shape (layers_dims[1], layers_dims[0])
-  // b1 -- bias vector of shape (layers_dims[1], 1)
-  // WL -- weight matrix of shape (layers_dims[L], layers_dims[L-1])
-  // bL -- bias vector of shape (layers_dims[L], 1)
-  // FIXME: add example of conv2d wight shapes
-  map< string, vector< cv::Mat > > parameters \
-  initializer::init_params_zeros( vector< vector< int > > layer_dims, vector< string > layer_types );
-  
+  init_params_zeros( vector< vector< int > > layer_dims, vector< string > layer_types );
   
   // Implements a n-layer neural network: LINEAR->RELU->LINEAR->RELU->....LINEAR->SIGMOID.
   // or: CONV->MAXPOOL->RELU->CONV->MAXPOOL->RELU->...LINEAR->SOFTMAX 
@@ -93,7 +87,6 @@ private:
   map< string, vector< cv::Mat > > parameters \
   model( vector< cv::Mat > X, vector< cv::Mat > Y, float learning_rate = 0.01, int num_iters= 15000, bool print_cost = True, string init = "he" );
   
-
 };
 
 #endif
