bc340c338dd05d38fbcd6154caa9f7462f82e04e
ZacharyIG <zacharyigreenberg@gmail.com>
2018-03-22 14:20:38 -0700

    Update optimizer.hpp

--- headers/optimizer.hpp
+++ headers/optimizer.hpp
@@ -1,3 +1,26 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Copyright 2018 Zachary Greenberg
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// File:    optimizer.hpp
+// Author:  Zachary Greenberg
+// Summary: Class optimization layer providing support for minimizing model
+//          optimization objectives (least squares, cross-entropy, softmax) 
+//          using several popular algorithms (Gradient-Descent, Stochastic Descent, 
+//          Mini-batch Gradient Descent, RMS Prop, Adam Optimization)
+//          Support for initializing weight vectors with zeros, random values,
+//          and He. Initialization is included.
+
 #include <stdio.h>
 #include <iostream>
 #include <vector>
@@ -17,14 +40,11 @@ using namespace std;
 
 class optimizer {
 
-  
 public:
 
-  
-  // init Constructor and destructor
+  // Constructor and destructor
   optimizer();
   virtual ~optimizer();
-
 
 private:
 
@@ -42,7 +62,6 @@ private:
   map< string, vector< cv::Mat > > update_parameters_gd( map< string, vector< cv::Mat > > parameteters, \
 						       map< string, vector< cv::Mat > > grads, float learning_rate );
 
-
   // Creates a list of random minibatches from (X, Y)  
   // Arguments:
   // X -- input data, of shape (input size, number of examples)
@@ -53,7 +72,6 @@ private:
   map< string, vector< cv::Mat > > rand_mini_batches( vector< cv::Mat > X, vector< cv::Mat > Y, \
 						         	    int mini_batch_size = 64, int seed = 0 );
   
-
 };
 
 #endif
