6899dc1654f2511d449fa77a3b4e9aea50cbdc2b
ZacharyIG <zacharyigreenberg@gmail.com>
2018-03-22 14:27:59 -0700

    updated hpp

--- headers/nnet.hpp
+++ headers/nnet.hpp
@@ -1,3 +1,24 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Copyright 2018 Zachary Greenberg
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// File:    nnet.hpp
+// Author:  Zachary Greenberg
+// Summary: nnet layer providing support for defining, training, and 
+//          testing models with Fully-Connected, Convolutional, or Recurrent
+//          network architecture. Several common activation functions such as
+//          Sigmoid(), Relu(), tanH(), and Softmax() are supported.
+
 #include <stdio.h>
 #include <iostream>
 #include <fstream>
